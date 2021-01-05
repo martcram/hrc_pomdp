a7753d9416d723e6eda55b1c8db3933a249ba748
Martijn Cramer <martijn.cramer@hotmail.com>
2021-01-05 17:34:01 +0100

    [AndOrGraph] implement functions for adding edges to and-or graph

--- include/AndOrGraph.hpp
+++ include/AndOrGraph.hpp
@@ -16,6 +16,9 @@ public:
     AndOrGraph();
     AndOrGraph(const std::vector<Node> &nodes, const std::vector<AndEdge> &edges);
     ~AndOrGraph() = default;
+
+    void add_edge(const AndEdge &edge);
+    void add_edges_from(const std::vector<AndEdge> &edges);
 };
 
 #endif // AND_OR_GRAPH_HPP
--- src/AndOrGraph.cpp
+++ src/AndOrGraph.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <vector>
 
 #include "AndEdge.hpp"
@@ -12,4 +13,16 @@ AndOrGraph::AndOrGraph()
 AndOrGraph::AndOrGraph(const std::vector<Node> &nodes, const std::vector<AndEdge> &edges)
     : nodes{nodes}, edges{edges}
 {
+}
+
+void AndOrGraph::add_edge(const AndEdge &edge)
+{
+    if (std::find(edges.begin(), edges.end(), edge) == edges.end())
+        edges.push_back(edge);
+}
+
+void AndOrGraph::add_edges_from(const std::vector<AndEdge> &edges)
+{
+    for (const auto &edge : edges)
+        this->add_edge(edge);
 }
