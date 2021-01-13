8830cba4091ae38fc763de80c678b71216379e7f
Martijn Cramer <martijn.cramer@hotmail.com>
2021-01-13 21:08:51 +0100

    [Assembly] create assembly class

---
+++ AndOrGraph/include/AndOrGraph/Assembly.hpp
@@ -0,0 +1,13 @@
+#ifndef ASSEMBLY_HPP
+#define ASSEMBLY_HPP
+
+class Assembly
+{
+private:
+    /* data */
+public:
+    Assembly(/* args */);
+    ~Assembly() = default;
+};
+
+#endif // ASSEMBLY_HPP
---
+++ AndOrGraph/src/AndOrGraph/Assembly.cpp
@@ -0,0 +1,5 @@
+#include <AndOrGraph/Assembly.hpp>
+
+Assembly::Assembly(/* args */)
+{
+}
