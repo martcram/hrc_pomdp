d4d3f53a252b64784044e65c649cf7a51b938374
Martijn Cramer <martijn.cramer@hotmail.com>
2021-01-16 16:57:01 +0100

    [math_utils] Cartesian prod: add template func. for product of vector<vector<T>> and vector<T>

--- math_utils/include/math_utils/math_utils.hpp
+++ math_utils/include/math_utils/math_utils.hpp
@@ -25,6 +25,26 @@ namespace math_utils
         return product;
     }
 
+    template <typename T>
+    std::vector<std::vector<T>> cartesian_product(const std::vector<std::vector<T>> &r1, const std::vector<T> &r2)
+    {
+        // If either of the sets is empty; the Cartesian product of those sets is also empty
+        if (r1.empty() || std::any_of(r1.begin(), r1.end(), [](const auto &item) { return item.empty(); }) || r2.empty())
+            return std::vector<std::vector<T>>{};
+
+        std::vector<std::vector<T>> product{};
+        for (auto i = r1.begin(); i != r1.end(); ++i)
+        {
+            for (auto j = r2.begin(); j != r2.end(); ++j)
+            {
+                std::vector<T> prod{*i};
+                prod.push_back(*j);
+                product.push_back(prod);
+            }
+        }
+        return product;
+    }
+
     // Along the lines of Anumi (2016): https://stackoverflow.com/questions/5279051
     template <typename T>
     std::vector<std::vector<T>> cartesian_product(const std::vector<std::vector<T>> &v)
@@ -33,7 +53,7 @@ namespace math_utils
         if (std::any_of(v.begin(), v.end(), [](const std::vector<T> &u) { return u.empty(); }))
             return std::vector<std::vector<T>>{};
 
-        std::vector<std::vector<T>> s {{}};
+        std::vector<std::vector<T>> s{{}};
         for (const auto &u : v)
         {
             std::vector<std::vector<T>> r;
