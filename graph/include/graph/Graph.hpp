d089f2c4900a1f3f8081e5e472262ec5a23de13b
Martijn Cramer <martijn.cramer@hotmail.com>
2021-01-19 15:37:25 +0100

    [Graph] check if node exists when requesting neighbors

--- graph/include/graph/Graph.hpp
+++ graph/include/graph/Graph.hpp
@@ -49,7 +49,11 @@ public:
 
     std::vector<T> get_neighbors(const T &u) const
     {
-        return adjacency_list.at(u);
+        auto it = adjacency_list.find(u);
+        if (it == adjacency_list.end())
+            return std::vector<T>{};
+        else
+            return it->second;
     }
 
     std::vector<T> get_nodes() const
