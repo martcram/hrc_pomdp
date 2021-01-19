cd5f5c4dc03f53dfb18813d2cdc96ed0e0661a59
Martijn Cramer <martijn.cramer@hotmail.com>
2021-01-19 10:15:01 +0100

    [graph] initialize undirected Graph class

--- graph/include/graph/Graph.hpp
+++ graph/include/graph/Graph.hpp
@@ -1,4 +1,18 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
+#include <unordered_map>
+#include <vector>
+
+template <typename T>
+class Graph
+{
+private:
+    std::unordered_map<T, std::vector<T>> adjacency_list;
+
+public:
+    Graph();
+    ~Graph() = default;
+};
+
 #endif // GRAPH_HPP
