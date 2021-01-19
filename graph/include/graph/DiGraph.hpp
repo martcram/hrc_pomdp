29b30ac9f1e1eeb3a8942af33fe4bf27f1004ff9
Martijn Cramer <martijn.cramer@hotmail.com>
2021-01-19 12:20:54 +0100

    [DiGraph] add adjacency list and overloaded constructor

--- graph/include/graph/DiGraph.hpp
+++ graph/include/graph/DiGraph.hpp
@@ -1,12 +1,27 @@
 #ifndef DIGRAPH_HPP
 #define DIGRAPH_HPP
 
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
 template <typename T>
 class DiGraph
 {
 private:
+    std::unordered_map<T, std::vector<T>> adjacency_list;
+
 public:
-    DiGraph();
+    DiGraph()
+        : adjacency_list{}
+    {
+    }
+
+    explicit DiGraph(const std::vector<std::pair<T, T>> &edges)
+        : DiGraph()
+    {
+    }
+
     ~DiGraph() = default;
 };
 
